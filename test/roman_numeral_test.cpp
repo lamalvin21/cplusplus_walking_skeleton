5c68fc1f6bca7d3483731bff97c812c6dfe429fb
Izaan Siddiqi <izaan.siddiqi@sky.uk>
2026-08-21 15:57:15 +0100

    Remove unnecessary local variables

--- test/roman_numeral_test.cpp
+++ test/roman_numeral_test.cpp
@@ -1,13 +1,10 @@
 #include <gtest/gtest.h>
 #include <roman_numeral.h>
-#include <string>
 
-TEST(RomanNumeral, one) {
-    std::string result = convertToRomanNumeral(1);
-    ASSERT_EQ("I", result);
+TEST(RomanNumeral, One) {
+    ASSERT_EQ("I", convertToRomanNumeral(1));
 }
 
 TEST(RomanNumeral, Two) {
-    const std::string result = convertToRomanNumeral(2);
-    ASSERT_EQ("II", result);
+    ASSERT_EQ("II", convertToRomanNumeral(2));
 }
