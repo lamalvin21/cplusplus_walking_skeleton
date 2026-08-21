7a0c0ba65d190210c0ebf6e7247ebf2782fc5c0d
Lam, Alvin (Engineer - Software Engineering - Sky) <alvin.lam@sky.uk>
2026-08-21 16:05:18 +0100

    reduce convertToRomanNumeral to use string fill constructor

--- src/roman_numeral.h
+++ src/roman_numeral.h
@@ -1,15 +1,5 @@
 #include <string>
 
 std::string convertToRomanNumeral(const int integer) {
-    if (integer == 2)
-    {
-        return "II";
-    }
-
-    if (integer == 3)
-    {
-        return "III";
-    }
-
-    return "I";
+    return std::string(integer, 'I');
 }
