940aff3873c12ed25ec299a0716f243930fda520
Lam, Alvin (Engineer - Software Engineering - Sky) <alvin.lam@sky.uk>
2026-08-25 10:17:04 +0100

    roman numerals: flip conditions in convertToRomanNumerals

--- src/roman_numeral.h
+++ src/roman_numeral.h
@@ -1,13 +1,15 @@
 #include <string>
 
 std::string convertToRomanNumeral(const int integer) {
-    if (integer < 4)
-    {
-        return std::string(integer, 'I');
-    }
     if (integer == 5)
     {
         return {"V"};
+    }    
+
+    if (integer == 4)
+    {
+        return {"IV"};
     }
-    return {"IV"};
+    
+    return std::string(integer, 'I');
 }
