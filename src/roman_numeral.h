57d877fcc0d9c8c4a0f5f8a7c96fd3ec2e74c6a6
Lam, Alvin (Engineer - Software Engineering - Sky) <alvin.lam@sky.uk>
2026-08-25 12:09:04 +0100

    add lowernumeral parameter to concatenateAdjacentNumerals function

--- src/roman_numeral.h
+++ src/roman_numeral.h
@@ -1,6 +1,6 @@
 #include <string>
 
-void concatenateAdjacentNumerals(const int higherInteger, const std::string &higherNumeral, const int lowerInteger, int &integer, std::string &result) {
+void concatenateAdjacentNumerals(const int higherInteger, const std::string &higherNumeral, const int lowerInteger, const std::string &lowerNumeral, int &integer, std::string &result) {
     while (integer >= higherInteger)
     {
         integer -= higherInteger;
@@ -10,7 +10,7 @@ void concatenateAdjacentNumerals(const int higherInteger, const std::string &hig
     if (integer >= higherInteger - lowerInteger)
     {
         integer -= higherInteger - lowerInteger;
-        result += "X";
+        result += lowerNumeral;
         result += higherNumeral;
     }
 }
@@ -18,7 +18,7 @@ void concatenateAdjacentNumerals(const int higherInteger, const std::string &hig
 std::string convertToRomanNumeral(int integer) {
     std::string result = {};
 
-    concatenateAdjacentNumerals(50, "L", 10, integer, result);
+    concatenateAdjacentNumerals(50, "L", 10, "X", integer, result);
 
     while (integer >= 10)
     {
